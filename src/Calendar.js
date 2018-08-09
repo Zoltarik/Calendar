2d840d8728d0febf7ce16dc552fbedf837636276
Zoltarik <zoltariki@gmail.com>
2018-08-09 22:07:27 +0300

    Initial commit

---
+++ src/Calendar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Calendar.css";
+
+const CellCalendar = () => {
+  return (
+    <div className="WrapDays">
+      <div className="WrapData">
+        <p className="numDay">1</p>
+        <p className="WorkShift">Утро</p>
+        <p className="Worker">Ринат</p>
+      </div>
+    </div>
+  );
+};
+
+const Calendar = () => {
+  return (
+    <div className="WrapDays">
+      <div className="WrapData">
+        <p className="numDay">1</p>
+        <p className="WorkShift">Утро</p>
+        <p className="Worker">Ринат</p>
+      </div>
+    </div>
+  );
+};
+export default Calendar;
