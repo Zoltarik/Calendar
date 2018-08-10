734731a8c85d63d25549d6bc2deedb53af3d3f71
Zoltarik <zoltariki@gmail.com>
2018-08-10 21:40:33 +0300

    zoltarik
    add month and day

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,26 +1,50 @@
 import React from "react";
 import "./Calendar.css";
 
-const CellCalendar = () => {
+const CellCalendar = (props) => {
   return (
     <div className="WrapDays">
       <div className="WrapData">
-        <p className="numDay">1</p>
+        <p className="numDay">{props.day}</p>
         <p className="WorkShift">Утро</p>
         <p className="Worker">Ринат</p>
+        <p className="WorkShift">Вечер</p>
+        <p className="Worker">Алексей</p>
       </div>
     </div>
   );
 };
 
 const Calendar = () => {
-  return (
+  let arrayMonth = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
+  let currentDate = new Date();
+  let numberMonth = arrayMonth[currentDate.getMonth()];
+  
+  const getNumberDays = (year, month) =>{
+    return new Date(year,month,0).getDay()
+  }
+
+  
+  return (  
     <div className="WrapMonth">
       <div className="selectMonth">
-        <p className="monthName">Август</p>
+        <p className="monthName">{numberMonth}</p>
       </div>
       <div className="placeDays">
-        <CellCalendar/>
+        <CellCalendar />
       </div>
     </div>
   );
